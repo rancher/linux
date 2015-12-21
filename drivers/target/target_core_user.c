2430ae62b3b8b26e00bb5f27fe87752299f42072
Max Khon <fjoe@samodelkin.net>
2015-12-21 20:00:02 +0000

    Remove async=%d token support. The device always supports async mode now.

--- drivers/target/target_core_user.c
+++ drivers/target/target_core_user.c
@@ -857,6 +857,7 @@ static struct se_device *tcmu_alloc_device(struct se_hba *hba, const char *name)
 	}
 
 	udev->hba = hba;
+	set_bit(TCMU_DEV_BIT_ASYNC, &udev->flags);
 
 	init_waitqueue_head(&udev->wait_cmdr);
 	spin_lock_init(&udev->cmdr_lock);
@@ -1142,14 +1143,13 @@ static void tcmu_free_device(struct se_device *dev)
 }
 
 enum {
-	Opt_dev_config, Opt_dev_size, Opt_hw_block_size, Opt_async, Opt_err,
+	Opt_dev_config, Opt_dev_size, Opt_hw_block_size, Opt_err,
 };
 
 static match_table_t tokens = {
 	{Opt_dev_config, "dev_config=%s"},
 	{Opt_dev_size, "dev_size=%u"},
 	{Opt_hw_block_size, "hw_block_size=%u"},
-	{Opt_async, "async=%u"},
 	{Opt_err, NULL}
 };
 
@@ -1210,27 +1210,6 @@ static ssize_t tcmu_set_configfs_dev_params(struct se_device *dev,
 				break;
 			}
 			dev->dev_attrib.hw_block_size = tmp_ul;
-			break;
-		case Opt_async:
-			if (test_bit(TCMU_DEV_BIT_OPEN, &udev->flags)) {
-				pr_err("failed to change async mode: device in use\n");
-				break;
-			}
-			arg_p = match_strdup(&args[0]);
-			if (!arg_p) {
-				ret = -ENOMEM;
-				break;
-			}
-			ret = kstrtoul(arg_p, 0, &tmp_ul);
-			kfree(arg_p);
-			if (ret < 0) {
-				pr_err("kstrtoul() failed for hw_block_size=\n");
-				break;
-			}
-			if (tmp_ul)
-				set_bit(TCMU_DEV_BIT_ASYNC, &udev->flags);
-			else
-				clear_bit(TCMU_DEV_BIT_ASYNC, &udev->flags);
 			break;
 		default:
 			break;
